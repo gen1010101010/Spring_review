9c8b25f5d940d8f734d17bfec41b6ad5e73df4bc
LEESJ <leesj2468@naver.com>
2025-07-17 01:48:27 +0900

    second commit

---
+++ hello-spring/src/main/java/hello/hello_spring/domain/Member.java
@@ -0,0 +1,22 @@
+package hello.hello_spring.domain;
+
+public class Member {
+    private long id;
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+}
